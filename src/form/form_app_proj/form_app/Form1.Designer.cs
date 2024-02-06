d6c7f2e23aa89e6fd39a1624f4b7e8fa9566ce38
marija.jolovic <46-2021@pmf.kg.ac.rs>
2024-02-06 13:53:43 +0100

    Dodat label za dodavanje novih paketa

--- src/form/form_app_proj/form_app/Form1.Designer.cs
+++ src/form/form_app_proj/form_app/Form1.Designer.cs
@@ -76,37 +76,40 @@ namespace form_app {
             this.input_firstname = new System.Windows.Forms.TextBox();
             this.input_lastname = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 51);
+            this.label1.Location = new System.Drawing.Point(8, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.Size = new System.Drawing.Size(97, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Register new client";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Location = new System.Drawing.Point(8, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Provider Name";
             // 
             // input_username
             // 
-            this.input_username.Font = new Font("Arial", 10, FontStyle.Regular);
-            this.input_username.AutoSize = false;
             this.input_username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input_username.Font = new System.Drawing.Font("Arial", 10F);
             this.input_username.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.input_username.Location = new System.Drawing.Point(10, 79);
-            this.input_username.MaximumSize = new System.Drawing.Size(150, 50);
-            this.input_username.MinimumSize = new System.Drawing.Size(10, 10);
+            this.input_username.Location = new System.Drawing.Point(8, 64);
+            this.input_username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.input_username.MaximumSize = new System.Drawing.Size(113, 41);
+            this.input_username.MinimumSize = new System.Drawing.Size(8, 8);
             this.input_username.Name = "input_username";
-            this.input_username.Size = new System.Drawing.Size(150, 30);
+            this.input_username.Size = new System.Drawing.Size(113, 25);
             this.input_username.TabIndex = 3;
             this.input_username.Text = "username";
             this.input_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -115,15 +118,15 @@ namespace form_app {
             // 
             // input_firstname
             // 
-            this.input_firstname.Font = new Font("Arial", 10, FontStyle.Regular);
-            this.input_firstname.AutoSize = false;
             this.input_firstname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input_firstname.Font = new System.Drawing.Font("Arial", 10F);
             this.input_firstname.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.input_firstname.Location = new System.Drawing.Point(170, 79);
-            this.input_firstname.MaximumSize = new System.Drawing.Size(150, 50);
-            this.input_firstname.MinimumSize = new System.Drawing.Size(10, 10);
+            this.input_firstname.Location = new System.Drawing.Point(128, 64);
+            this.input_firstname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.input_firstname.MaximumSize = new System.Drawing.Size(113, 41);
+            this.input_firstname.MinimumSize = new System.Drawing.Size(8, 8);
             this.input_firstname.Name = "input_firstname";
-            this.input_firstname.Size = new System.Drawing.Size(150, 30);
+            this.input_firstname.Size = new System.Drawing.Size(113, 25);
             this.input_firstname.TabIndex = 4;
             this.input_firstname.Text = "First name";
             this.input_firstname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -132,49 +135,55 @@ namespace form_app {
             // 
             // input_lastname
             // 
-            this.input_lastname.Font = new Font("Arial", 10, FontStyle.Regular);
-            this.input_lastname.AutoSize = false;
             this.input_lastname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input_lastname.Font = new System.Drawing.Font("Arial", 10F);
             this.input_lastname.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.input_lastname.Location = new System.Drawing.Point(330, 79);
-            this.input_lastname.MaximumSize = new System.Drawing.Size(150, 50);
-            this.input_lastname.MinimumSize = new System.Drawing.Size(10, 10);
+            this.input_lastname.Location = new System.Drawing.Point(248, 64);
+            this.input_lastname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.input_lastname.MaximumSize = new System.Drawing.Size(113, 41);
+            this.input_lastname.MinimumSize = new System.Drawing.Size(8, 8);
             this.input_lastname.Name = "input_lastname";
-            this.input_lastname.Size = new System.Drawing.Size(150, 30);
+            this.input_lastname.Size = new System.Drawing.Size(113, 25);
             this.input_lastname.TabIndex = 5;
             this.input_lastname.Text = "Last name";
             this.input_lastname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.input_lastname.Enter += new System.EventHandler(this.input_textbox_Enter);
             this.input_lastname.Leave += new System.EventHandler(this.input_lastname_Leave);
             // 
-            // button_register
+            // button1
             // 
-            this.button1.AutoSize = false;
-            this.button1.Location = new System.Drawing.Point(490, 79);
-            this.button1.Name = "button_register";
-            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(368, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
             this.button1.TabIndex = 6;
             this.button1.Text = "Register";
             this.button1.UseVisualStyleBackColor = true;
-
-            // Set FlatStyle to Flat
-            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-
-            // Customize FlatAppearance for a square border
-            button1.FlatAppearance.BorderSize = 1; // Set the border size
-            button1.FlatAppearance.BorderColor = System.Drawing.Color.Black; // Set the border color
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Add new package";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.ClientSize = new System.Drawing.Size(600, 390);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.input_lastname);
             this.Controls.Add(this.input_firstname);
             this.Controls.Add(this.input_username);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "ProviderApp";
             this.ResumeLayout(false);
@@ -190,6 +199,7 @@ namespace form_app {
         private System.Windows.Forms.TextBox input_firstname;
         private System.Windows.Forms.TextBox input_lastname;
         private Button button1;
+        private Label label3;
     }
 }
 
